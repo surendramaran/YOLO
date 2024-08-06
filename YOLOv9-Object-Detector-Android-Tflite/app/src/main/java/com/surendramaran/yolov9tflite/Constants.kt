442fceccdc87a2302d09737c1feec33b9c9b9c4b
Surendra Maran <surendramaran8@gmail.com>
2024-08-06 22:34:38 +0530

    fix mistake

--- YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/Constants.kt
+++ YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov9tflite
 
 object Constants {
-    const val MODEL_PATH = "wood.tflite"
+    const val MODEL_PATH = "v9.tflite"
     const val LABELS_PATH = "labels.txt"
 }
